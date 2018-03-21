b7ab5ccfbe4bc7074aae1d8857d90faac064d853
Oliver Becher <oliver.becher@gmail.com>
2018-03-21 10:37:05 +0100

    Save search results by tweet, pick up search after it hast stopped

--- 0_data_retrieval/get_tweet_search_results.py
+++ 0_data_retrieval/get_tweet_search_results.py
@@ -30,19 +30,28 @@ def user_decoder(obj):
                 obj['controversy'])
 
 
+def skip_pre_searched_tweets(user):
+    with open(DIR + 'user_tweet_web_search/' + 'user_' + str(user.user_id) + '.json', 'r') as in_file:
+        pre_crawled_docs = [json.loads(line) for line in in_file.readlines()]
+        pre_crawled_docs = [d['text'] for d in pre_crawled_docs if 'text' in d]
+    tweets_to_be_searched = []
+    for t in user.tweets:
+        if t['text'] not in pre_crawled_docs:
+            tweets_to_be_searched.append(t)
+    user.tweets = tweets_to_be_searched
+    return user
+
+
 def get_data():
-    pre_crawled_files = [user_file for user_file in glob.glob(DIR + 'user_tweet_web_search/' + 'user_*.json') if 'user_' in user_file]
-    pre_crawled_files = [user_file[user_file.rfind('/')+1:] for user_file in pre_crawled_files]
     user_files = glob.glob(DIR + 'user_tweets/' + 'user_*.json')
     for user_file in user_files:
-        if user_file in pre_crawled_files: continue
         user = json.loads(open(user_file).readline(), object_hook=user_decoder)
         yield user
 
 
 def parse_links_in_tweet(tweet):
     tweet_text = tweet['text']
-    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', tweet_text)
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tweet_text)
     for url in urls:
         if 'https://t.co/' in url:
             tweet_text = tweet_text.replace(url, '')
@@ -79,46 +88,93 @@ def get_web_doc_sentences(url):
 def get_bing_documents_for_tweet(user):
     search_url = "https://api.cognitive.microsoft.com/bing/v7.0/search"
     print('Web search for: {}'.format(user.user_id))
-    tweets_with_docs = []
-    # todo: save user once, then each tweet with docs on one line
+    # Create user file if it does not exist
+    if user.user_id not in pre_crawled_files:
+        create_user_file(user)
     for tweet in user.tweets:
         # If tweet contains link to another tweet, link should be resolved and tweet parsed..
         search_term = parse_links_in_tweet(tweet)
         print(search_term)
 
         headers = {"Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY}
-        params = {"q": search_term, "textDecorations": True, "textFormat": "HTML", "answerCount": 30, "promote": "News"}
-        # params  = {"q": search_term, "textDecorations":True, "textFormat":"HTML", "count": 30, "promote": "News"}
+        params = {"q": search_term, "textDecorations": True, "textFormat": "HTML", "answerCount": 1, "promote": "News", "mkt": "en-US"}
         response = requests.get(search_url, headers=headers, params=params)
         response.raise_for_status()
         search_results = response.json()
-        found_pages = search_results['webPages']['value'] if 'webPages' in search_results else None
+        relevant_pages = search_results['webPages']['value'] if 'webPages' in search_results else None
+        # user.tweets <text, created_at, quoted_status, docs>
         docs_formatted = [{
                               'url': doc['url'],
                               'content': get_web_doc_sentences(doc['url'])
-                          } for doc in found_pages] if found_pages is not None else []
-        web_documents = tweet
-        web_documents['docs'] = docs_formatted
-        tweets_with_docs.append(web_documents)
-    user.tweets = tweets_with_docs
-    store_result(user)
+                          } for doc in relevant_pages] if relevant_pages is not None else []
+        tweet['docs'] = docs_formatted
+        # It would be easier to find unigrams and bigrams right away instead of storing the whole document
+        for doc in docs_formatted:
+            if doc['content']: get_ngram_snippets(tweet, doc['content'])
+        # Append search result to user file
+        store_search_result(user.user_id, tweet)
 
 
-def calculate_similarity(tweet, web_documents):
-    ungrams = nltk.sent_tokenize(tweet)
-    possible_snippets = [''.join(web_documents[i:i + 4]) for i in range(len(web_documents) - 4)]
-    pass
+def get_ngram_snippets(tweet, web_document):
+    # web doc in sentences
+    # Todo: lemmatization on tweet & doc to find better unigrames / bigrams?
+    uni_tweet_tokens = nltk.word_tokenize(tweet['text'])
+    bi_tweet_tokens = [' '.join(uni_tweet_tokens[i:i + 2]) for i in range(len(uni_tweet_tokens) - 1)]
+
+    if len(web_document) < 4:
+        doc_snippets = ' '.join(web_document)
+    else:
+        doc_snippets = [' '.join(web_document[i:i + 4]) for i in range(len(web_document) - 3)]
+    print(doc_snippets)
+    OVERLAP_THRESHOLD = 0.4
+
+    unigram_snippets = []
+    bigram_snippets = []
+    for snip in doc_snippets:
+        cnt_u = 0
+        cnt_b = 0
+        for unigr in uni_tweet_tokens:
+            if unigr in snip: cnt_u += 1
+        overlap_score_u = cnt_u / len(uni_tweet_tokens)
+        if overlap_score_u >= OVERLAP_THRESHOLD:
+            print("Uni Overlap: {}".format(snip))
+            unigram_snippets.append([snip, overlap_score_u])
+        for bigr in bi_tweet_tokens:
+            if bigr in snip: cnt_b += 1
+        overlap_score_b = cnt_b / len(bi_tweet_tokens)
+        if overlap_score_b >= OVERLAP_THRESHOLD:
+            print("Bi Overlap: {}".format(snip))
+            bigram_snippets.append([snip, overlap_score_b])
+
+    # Unigrams<Snippets<text, score>>, Bigrams<Snippets<text, score>>
+    print(len(unigram_snippets))
+    print(len(bigram_snippets))
+    return unigram_snippets, bigram_snippets
 
 
-def store_result(user):
+def create_user_file(user):
     with open(DIR + 'user_tweet_web_search/' + 'user_' + str(user.user_id) + '.json', 'w') as out_file:
         out_file.write(json.dumps(user.__dict__, default=datetime_converter) + '\n')
 
 
+def store_search_result(user_id, tweet):
+    with open(DIR + 'user_tweet_web_search/' + 'user_' + str(user_id) + '.json', 'a') as out_file:
+        out_file.write(json.dumps(tweet, default=datetime_converter) + '\n')
+
+
 def main():
+    global pre_crawled_files
+    pre_crawled_files = [user_file for user_file in glob.glob(DIR + 'user_tweet_web_search/' + 'user_*.json') if
+                         'user_' in user_file]
+    pre_crawled_files = [user_file[user_file.rfind('_') + 1:user_file.rfind('.')] for user_file in pre_crawled_files]
     users = get_data()
-    #Parallel(n_jobs=NUM_CORES)(delayed(get_bing_documents_for_tweet)(user) for user in users)
-    for user in users: get_bing_documents_for_tweet(user)
+    # Can we parallelize this pls?
+    # Parallel(n_jobs=NUM_CORES)(delayed(get_bing_documents_for_tweet)(user) for user in users)
+    for user in users:
+        if str(user.user_id) in pre_crawled_files:
+            # Get user file, skip tweets that have been searched for already
+            user = skip_pre_searched_tweets(user)
+        get_bing_documents_for_tweet(user)
 
 
 if __name__ == "__main__":
