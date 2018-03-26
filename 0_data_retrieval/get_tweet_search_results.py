285e2da11cf1e80497385b65df49ea1a25b29689
Oliver Becher <oliver.becher@gmail.com>
2018-03-26 18:59:09 +0200

    Restructure search processingÄ

--- 0_data_retrieval/get_tweet_search_results.py
+++ 0_data_retrieval/get_tweet_search_results.py
@@ -161,28 +161,29 @@ def get_bing_documents_for_tweet(user):
             search_terms.append(query_term)
 
     print(len(search_terms))
-    results = make_web_query(search_terms, user.user_id)
+    return tweets, make_web_query(search_terms, user.user_id)
 
-    for tweet in tweets:
-        tweet['snippets'] = []
-        if tweet['search_instance']:
-            search_instance = tweet['search_instance']
-            del tweet['search_instance']
-            query_term = results[search_instance]['query']
-            relevant_pages = [result['link'] for result in results[search_instance]]
 
-            if relevant_pages: print('Found results: {}'.format(len(relevant_pages)))
-            docs_formatted = [{
-                                  'url': url,
-                                  'content': get_web_doc(url)
-                              } for url in relevant_pages] if len(relevant_pages) > 0 else []
-            # Parse unigram and bigram overlaps of search results with query
-            # user.tweets <text, created_at, quoted_status, snippets<unigrams, bigrams, url>>
-            for doc in docs_formatted:
-                if doc['content']:
-                    tweet['snippets'].append(get_ngram_snippets(query_term, doc['content'], doc['url']))
-        # Append search result to user file
-        store_search_result(user.user_id, tweet)
+def extract_tweet_search_results(tweet, results, user_id):
+    tweet['snippets'] = []
+    if tweet['search_instance']:
+        search_instance = tweet['search_instance']
+        del tweet['search_instance']
+        query_term = results[search_instance]['query']
+        relevant_pages = [result['link'] for result in results[search_instance]]
+
+        if relevant_pages: print('Found results: {}'.format(len(relevant_pages)))
+        docs_formatted = [{
+                              'url': url,
+                              'content': get_web_doc(url)
+                          } for url in relevant_pages] if len(relevant_pages) > 0 else []
+        # Parse unigram and bigram overlaps of search results with query
+        # user.tweets <text, created_at, quoted_status, snippets<unigrams, bigrams, url>>
+        for doc in docs_formatted:
+            if doc['content']:
+                tweet['snippets'].append(get_ngram_snippets(query_term, doc['content'], doc['url']))
+
+    store_search_result(user_id, tweet)
 
 
 def get_ngram_snippets(tweet_text, web_document, url):
@@ -252,8 +253,12 @@ def query_manager(user):
         user = skip_pre_searched_tweets(user)
     if len(user.tweets) == 0:
         return
-    get_bing_documents_for_tweet(user)
-    return
+
+    # Get documents from bing search related to tweet
+    tweets, results = get_bing_documents_for_tweet(user)
+
+    # Extract documents and do uni/bi-gram matching and store results
+    Parallel(n_jobs=NUM_CORES)(delayed(extract_tweet_search_results)(tweet, results, user.user_id) for tweet in tweets)
 
 
 def main():
@@ -265,8 +270,8 @@ def main():
         map(int, [user_file[user_file.rfind('_') + 1:user_file.rfind('.')] for user_file in pre_crawled_files]))
     users = get_data()
     # Can we parallelize this pls?
-    Parallel(n_jobs=NUM_CORES, backend="threading")(delayed(query_manager)(user) for user in users)
-    # for user in users: query_manager(user)
+
+    for user in users: query_manager(user)
 
 if __name__ == "__main__":
     main()
