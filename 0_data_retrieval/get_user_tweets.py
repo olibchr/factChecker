74672de027e82f4335a5aa4517dd9b2d62a76531
Oliver Becher <oliver.becher@gmail.com>
2018-03-29 12:24:02 +0200

    Add comment to later scan files for duplicate tweets..

--- 0_data_retrieval/get_user_tweets.py
+++ 0_data_retrieval/get_user_tweets.py
@@ -88,6 +88,7 @@ def get_user_tweets(api, transactions, user_files):
 
 
 def store_result(user):
+    # TODO: was previosuly append, need to scan files and make sure we dont have duplicates
     with open(DIR + 'user_tweets/' + 'user_' + str(user.user_id) + '.json', 'w') as out_file:
         out_file.write(json.dumps(user.__dict__, default=datetime_converter) + '\n')
 
