abbe7eb85c79b178ae5106978af97268b030034c
Oliver Becher <oliver.becher@gmail.com>
2018-04-19 10:32:57 +0200

    only get web docs

--- 0_data_retrieval/get_web_docs.py
+++ 0_data_retrieval/get_web_docs.py
@@ -45,12 +45,7 @@ NLTK_STOPWORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', '
 
 OVERLAP_THRESHOLD = 0.4
 query_dir = DIR + "user_tweet_query_mod/*_search_results.csv"
-out_dir = DIR + "user_snippets/"
-
-
-# if not SERVER_RUN: search_engine_files = DIR + "user_tweet_query_mod/14*_results.csv"
-# if not SERVER_RUN: out_dir = DIR + 'user_snippets/'
-
+out_dir = DIR + "user_docs/"
 
 def get_data():
     query_files = glob.glob(query_dir)
@@ -80,60 +75,6 @@ def get_data():
         df = pd.read_csv(qfile, index_col=False)
         df['userId'] = int(qfile[qfile.rfind('/') + 1:qfile.rfind('_search')])
         yield [df, int(qfile[qfile.rfind('/') + 1:qfile.rfind('_search')])]
-
-
-def get_ngram_snippets(tweet_text, web_document, url):
-    from nltk.stem import WordNetLemmatizer
-    from nltk import word_tokenize, sent_tokenize
-    if web_document is None or tweet_text is None: return None
-    WNL = WordNetLemmatizer()
-    # Remove odd characters from tweet, to lowercast and remove stopword. Parse into uni and bigrams
-    tweet_text = re.sub(r'[^a-z0-9]', ' ', tweet_text.lower())
-
-    # Lemamtization of tweet, remove stopwords, put into uni and bigrams
-    uni_tweet_tokens = [WNL.lemmatize(i) for i in word_tokenize(tweet_text) if i not in NLTK_STOPWORDS]
-    bi_tweet_tokens = [' '.join(uni_tweet_tokens[i:i + 2]) for i in range(len(uni_tweet_tokens) - 1)]
-    if len(uni_tweet_tokens) == 0:
-        return url, {'unigrams': [],
-                     'bigrams': [],
-                     'url': url
-                     }
-    if len(bi_tweet_tokens) == 0: bi_tweet_tokens = uni_tweet_tokens
-
-    # Split doc into senctences, lemmatize, remove stopwords and to lowercase
-    doc_sents = sent_tokenize(web_document)
-    doc_sents = [" ".join([WNL.lemmatize(i) for i in word_tokenize(sent) if i not in NLTK_STOPWORDS])
-                     .replace('\n', ' ')
-                 for sent in doc_sents]
-
-    if len(doc_sents) < 4:
-        doc_snippets = ' '.join(doc_sents)
-    else:
-        doc_snippets = [' '.join(doc_sents[i:i + 4]) for i in range(len(doc_sents) - 3)]
-
-    unigram_snippets = []
-    bigram_snippets = []
-    for snip in doc_snippets:
-        cnt_u, cnt_b = 0, 0
-        for unigr in uni_tweet_tokens:
-            if unigr in snip: cnt_u += 1
-        overlap_score_u = cnt_u / len(uni_tweet_tokens)
-        if overlap_score_u >= OVERLAP_THRESHOLD:
-            unigram_snippets.append([snip, overlap_score_u])
-        for bigr in bi_tweet_tokens:
-            if bigr in snip: cnt_b += 1
-        overlap_score_b = cnt_b / len(bi_tweet_tokens)
-        if overlap_score_b >= OVERLAP_THRESHOLD:
-            bigram_snippets.append([snip, overlap_score_b])
-
-            # Unigrams<Snippets<text, score>>, Bigrams<Snippets<text, score>>
-            # if len(unigram_snippets) > 0:
-            # print('Unigrams: {}; bigrams: {}; UrL: {}'.format(len(unigram_snippets), len(bigram_snippets), url))
-
-    return url, {'unigrams': unigram_snippets,
-                 'bigrams': bigram_snippets,
-                 'url': url
-                 }
 
 
 def parallel_retrieval(urls):
@@ -167,7 +108,7 @@ def parallel_retrieval(urls):
     except KeyboardInterrupt:
         sys.exit(1)
     # print(Counter([r.status_code for r in responses.values()]))
-    del(q)
+    del (q)
     return responses
 
 
@@ -203,20 +144,6 @@ def get_web_doc(url, urlcontent):
         return None
 
 
-def extract_query_term(tweet):
-    if not tweet: return ""
-    tweet_text = tweet['text']
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tweet_text)
-    for url in urls:
-        if 'https://t.co/' in url:
-            tweet_text = tweet_text.replace(url, '')
-    if 'quoted_status' in tweet and tweet['quoted_status']:
-        tweet_text = tweet_text + ' ' + tweet['quoted_status']
-    if tweet_text[:2].lower() == 'rt': tweet_text = tweet_text[2:]
-    tweet_text = tweet_text.replace('\0', '').replace('\n', ' ')
-    return hashlib.md5(tweet_text.encode()).hexdigest()
-
-
 def get_tweet_search_results(df, userId):
     print("Working on {} with {} entries".format(userId, df.shape))
     if df.shape[0] < 1: return
@@ -242,44 +169,15 @@ def get_tweet_search_results(df, userId):
     df['content'].replace('', np.nan, inplace=True)
     df.dropna(subset=['content'], inplace=True)
 
-    print("Extracting snippets")
-    ngram_params = zip(df['query'].tolist(), df['content'].tolist(), df['link'].tolist())
-    url_snippets = Parallel(n_jobs=num_jobs)(delayed(get_ngram_snippets)(e[0], e[1], e[2]) for e in ngram_params)
-    url_snippets = {unit[0]: unit[1] for unit in url_snippets if unit is not None}
-
-    df['snippets'] = df['link'].map(lambda x: url_snippets[x] if x in url_snippets else '')
     df['hash'] = df['query'].map(lambda query: "" if query is None else hashlib.md5(query.encode()).hexdigest())
-
-    df.drop('content', axis=1, inplace=True)
 
     print("Finished {} with {} entries".format(userId, df.shape))
     with open(out_dir + str(userId) + '_snippets.json', 'w') as f:
         f.write(df.to_json(orient='records'))
-    del(df)
+    del (df)
 
 
 dfs = get_data()
 
 for df in dfs:
     get_tweet_search_results(df[0], df[1])
-# Parallel(n_jobs=num_jobs)(delayed(get_tweet_search_results)(df[0], df[1]) for df in dfs)
-
-# root
-#  |-- domain: string (nullable = true)
-#  |-- effective_query: string (nullable = true)
-#  |-- link: string (nullable = true)
-#  |-- link_type: string (nullable = true)
-#  |-- no_results: string (nullable = true)
-#  |-- num_results: string (nullable = true)
-#  |-- num_results_for_query: string (nullable = true)
-#  |-- page_number: string (nullable = true)
-#  |-- query: string (nullable = true)
-#  |-- rank: string (nullable = true)
-#  |-- requested_at: string (nullable = true)
-#  |-- requested_by: string (nullable = true)
-#  |-- scrape_method: string (nullable = true)
-#  |-- search_engine_name: string (nullable = true)
-#  |-- snippet: string (nullable = true)
-#  |-- status: string (nullable = true)
-#  |-- title: string (nullable = true)
-#  |-- visible_link: string (nullable = true)
