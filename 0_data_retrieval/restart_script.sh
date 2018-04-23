64a476d71433e91a5b94c752cf5e1f2185a1af10
Oliver Becher <oliver.becher@gmail.com>
2018-04-23 18:25:58 +0200

    F

--- 0_data_retrieval/restart_script.sh
+++ 0_data_retrieval/restart_script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 /var/scratch/obr280/0_Thesis/2_Code/0_data_retrieval/get_web_docs.py $1
+PRESET=$1
+python3 /var/scratch/obr280/0_Thesis/2_Code/0_data_retrieval/get_web_docs.py $PRESET
