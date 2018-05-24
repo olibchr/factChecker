bf742056f0c251b57aa61b3d1257dee49559d2fc
Oliver Becher <oliver.becher@gmail.com>
2018-05-24 09:56:14 -0500

    SKip counter

--- Castillo/0_data_retrieval/get_user_tweets.py
+++ Castillo/0_data_retrieval/get_user_tweets.py
@@ -85,8 +85,10 @@ def get_user_tweets(api, transactions, user_files):
                 user_tweets.append(parsed_status)
             # <user_id, tweets, fact, transactions, credibility, controversy>
             this_user = User(user_id, transactions=tr, tweets=user_tweets, features=user_features)
-            user_files.append(str(user_id))
+            this_user = was_user_correct(this_user)
+            store_result(this_user)
             print('Got tweets for user: {}, found: {}'.format(user_id, len(this_user.tweets)))
+            user_files.append(str(user_id))
             yield this_user
         except tweepy.error.TweepError as e:
             print('Twitter error: {}'.format(e))
@@ -127,7 +129,7 @@ def was_user_correct(user, facts, transactions):
             else:
                 user.was_correct = -1
             print(fact.true, transaction.stance, user.was_correct)
-    yield user
+    return user
 
 
 def store_result(user):
@@ -149,10 +151,10 @@ def main():
     api = tweepy.API(auth)
     users = get_user_tweets(api, transactions, user_files)
     #users = get_users()
-    # users = [was_user_correct(user, facts, transactions) for user in users]
-    print(Counter([u.was_correct for u in users]))
-    for user in users:
-        store_result(user)
+    #users = [was_user_correct(user, facts, transactions) for user in users]
+    # print(Counter([u.was_correct for u in users]))
+    #for user in users:
+    #    store_result(user)
 
 
 if __name__ == "__main__":
