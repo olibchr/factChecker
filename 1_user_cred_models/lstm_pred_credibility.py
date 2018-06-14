bf81aca1bd9e68219d1feacdaae480055e4c21ee
Oliver Becher <oliver.becher@gmail.com>
2018-06-14 21:35:03 +0200

    Dont put model in function

--- 1_user_cred_models/lstm_pred_credibility.py
+++ 1_user_cred_models/lstm_pred_credibility.py
@@ -473,7 +473,19 @@ def lstm_pred(n = 0):
     X_train = sequence.pad_sequences(X_train, maxlen=max_tweet_length)
     X_test = sequence.pad_sequences(X_test, maxlen=max_tweet_length)
 
-    model = get_trained_model(X_train, X_test, y_train, y_test)
+    # create the model
+    embedding_vecor_length = 32
+    model = Sequential()
+    model.add(Embedding(top_words, embedding_vecor_length, input_length=max_tweet_length))
+    model.add(Dropout(0.2))
+    model.add(LSTM(100))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=64)
+
+    #model = get_trained_model(X_train, X_test, y_train, y_test)
 
     # Final evaluation of the model
     scores = model.evaluate(X_test, y_test, verbose=0)
