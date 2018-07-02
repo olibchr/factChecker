2dc7da008ac36a5e61c083057d2b5e361326523f
Oliver Becher <oliver.becher@gmail.com>
2018-07-02 09:38:33 +0200

    Test train split

--- 5_fact_checking_models/cred_based_pred.py
+++ 5_fact_checking_models/cred_based_pred.py
@@ -232,8 +232,8 @@ def main():
         X, y, user_order = lstm_cred.get_prebuilt_data()
         X, y, user_order = lstm_cred.balance_classes(X, y, user_order)
         #X_train, X_test, y_train, y_test = train_test_split_on_facts(X, y, user_order, facts_train.values, users)
-        #X_train, X_test, y_train, y_test = lstm_cred.train_test_split_on_users(X, y, user_order, users, 100)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+        X_train, X_test, y_train, y_test = lstm_cred.train_test_split_on_users(X, y, user_order, users, 100)
+        #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
         X_train, X_test, word_to_idx = lstm_cred.keep_n_best_words(X_train, y_train, X_test, y_test, idx_to_word, top_words)
         max_tweet_length = 12
         X_train = sequence.pad_sequences(X_train, maxlen=max_tweet_length)
